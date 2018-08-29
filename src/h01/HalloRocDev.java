501107247494d3125bfb77c7c24176950f1fced6
kjao <kaj@4xrooze.nl>
2018-08-29 13:59:49 +0100

    message

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -6,6 +6,6 @@ package h01;
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("22222");
+        System.out.println("AAAAAAAAAAAAAAA");
     }
 }
